d4a7719d40dcbdf2d3dd1ea91b8ee99b6f9d1534
Sean Billideau <sbillideau@comcast.net>
2017-03-11 17:25:27 -0800

    Update CS225_Lab4.java

--- CS225_Lab4.java
+++ CS225_Lab4.java
@@ -1,56 +1,4 @@
-/* CS 225 - Intro to Computer Science
- * File Name: CS225_Lab4.java
- * Java Programming
- * Lab 4 - Due X/XX/XXXX
- * Instructor: Dr. Dan Grissom
- * 
- * Name 1: Sean Billideau
- * Name 2: 
- * Description: This file contains the source code for Lab 4.
- */
 
-///////////////////////////////////////////////////////////////////////////////
-// INSTRUCTIONS: Update the header above EACH LAB with the correct due date,
-// first/last names (remove the "Name 2" line if working alone) and description
-// for this specific lab. You should also update the first/last names and problem
-// number (if relevant) below in the "System.out.println" statement at the beginning
-// of the "main()" method. Failure to do so will result in lost points. DO NOT change
-// the name of the class or the autograder will give you 0 points.
-//
-// PHILOSOPHY: Lab is a chance to "get your feet wet" in a safe environment as it
-// will be the first time you'll be trying to program new concepts. Thus, as you'll
-// see below, the collaboration rules for LABS are very generous since the main idea
-// is for you to learn programming with a lot of resources. To foster this 
-// environment, you'll have access to a programming partner of your choice, your peers,
-// experienced lab technicians and your instructor.
-//
-// COLLABORATION: Students may officially work with ONE (1) partner. Both names should
-// be listed in the header and in the initial "System.out.println()" statement. When
-// you submit your lab, only one partner should submit it (again, make sure both names
-// are on all files). Students MAY seek advice and look at other students' code DURING
-// lab for tips (including students who are not your direct partner), but may NOT
-// copy/paste code from students other than your official partner.
-//
-// FINISHING & GRADING: Lab assignments must be performed in the Computer Science
-// Laboratory each week. You are required to attend every lab session. Labs usually
-// consist of several book problems (sometimes there are none) and 1-2 code problems.
-// The book problems and code should both be turned in when fully completed. 
-//
-// When you finish a code problem during lab, you will demonstrate your working program
-// to the instructor or to a lab-tech/TA on duty and you and the instructor will both
-// sign off on that problem. At that point, you will receive full credit for that problem.
-// If you did not finish one or more code problems by the end of lab, you may sign off with
-// an instructor/lab-tech/TA during the last 15 minutes of lab; in this event, you will not
-// be deducted points for not finishing , but will be graded based on your final
-// submission. Book problems will always be graded via the submission and will not be checked
-// off during lab.
-//
-// If you do not finish your lab assignment during the lab period, you and your partner may
-// only seek help with your code from lab technicians (available whenever the lab is open)
-// and the instructor to aid with any confusion; please refer to posted lab times at the
-// entrance of the Main Computer Lab to see when lab techs and computers are available.
-// You may also complete the labs on your own machine if youd like (this is preferable).
-///////////////////////////////////////////////////////////////////////////////
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -77,7 +25,7 @@ public class CS225_Lab4
 		
 		gameIsOver = false; // New game, so game is not over!
 		initializeRandomPrizeBoard();
-		// TODO 6: Instantiate offerHistory using the new keyword
+		
 		
 	}
 
@@ -119,8 +67,7 @@ public class CS225_Lab4
 		sum *= 0.85;
 		return sum;
 		
-		// TODO 2: Write code which returns the banker's offer as a double,
-		// according to the description in this method's comment above.
+		
 	}
 
 	/////////////////////////////////////////////////////////////////////
@@ -143,9 +90,7 @@ public class CS225_Lab4
 					
 			// This block is executed when the player accepts the banker's offer. Thus the game is over.
 			
-			// TODO 3: Set the gameIsOver variable to true
-			// Inform the user that the game is over and how much money they accepted from the banker.
-			// Print the offer history (there is a method to call for this).
+			
 		}
 		else
 		{
@@ -166,11 +111,7 @@ public class CS225_Lab4
 			
 			printOfferHistory((remainingPrizesBoard.get(0)));
 		}
-			// TODO 5: Set the gameIsOver variable to true
-			// Let the user know what prize they won behind the final door!
-			// Print the offer history (there is a method to call for this).
-		//else {printOfferHistory(0.0);
-		
+			
 	}
 		
 	/////////////////////////////////////////////////////////////////////
@@ -212,29 +153,10 @@ public class CS225_Lab4
 			System.out.println("You won more than the bankers offer which was " + df.format(max));
 			
 		}
-		// TODO 8: print out the banker offer history (will need to loop through
-		// your offerHistory member variable) and find the max offer made.
-		// Print one offer per line, like:
-		// Offer 1: $10.00
-		// Offer 2: $5.00
-		// .....
-		;		
-		// TODO 9: If the max offer was greater than the accepted prize, then print out
-		// the $$$ left out on the table (see the example in this method's header above).
-		// Otherwise, congratulate the user that they won more than the banker's max
-		// offer and display the banker's max offer.
+		
 	}
 	
-	
-	/////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////////////////////
-	/////////////DO NOT EDIT ANY PORTIONS OF METHODS BELOW///////////////
-	/////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////////////////////
+
 	
 	
 	/////////////////////////////////////////////////////////////////////
